01f5263e85e7ee0bdb2700ce4662022dd2f16b6c
Brendan <brendansbortz@protonmail.com>
2022-08-09 18:04:16 -0400

    better

--- .config/sway/status.fish
+++
@@ -1,8 +0,0 @@
-#!/bin/fish
-
-set battery (acpi -b)
-set uptime_formatted (uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
-set date_formatted (date +'%-d/%-m/%Y %H:%M:%S')
-set linux_version (uname -r | cut -d '-' -f1)
-
-echo "[$battery]" "[$uptime_formatted]"  "[$linux_version]"  "[$date_formatted]"
