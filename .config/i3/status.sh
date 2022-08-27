a5d7dfddec20f1258cbcd3adeb040dd7a5094f37
Brendan Bortz <brendansbortz@protonmail.com>
2022-08-27 08:53:22 -0400

    better

--- .config/i3/status.sh
+++ .config/i3/status.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-battery=$(acpi -b)
+cpus=$(lscpu | sed "6q;d" | sed "s/:[ ^]*0/: 0/g")
 uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
 date_formatted=$(date +'%-d/%-m/%Y %H:%M:%S')
 linux_version=$(uname -r | cut -d '-' -f1)
