577b81fe82b5ec21ec0988a0b11a9a4734b4bcfd
Brendan Bortz <brendansbortz@protonmail.com>
2022-08-27 08:54:35 -0400

    better

--- .config/i3/status.sh
+++ .config/i3/status.sh
@@ -5,4 +5,4 @@ uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
 date_formatted=$(date +'%-d/%-m/%Y %H:%M:%S')
 linux_version=$(uname -r | cut -d '-' -f1)
 
-echo "[$battery]" "[$uptime_formatted]"  "[$linux_version]"  "[$date_formatted]"
+echo "[$cpus]" "[$uptime_formatted]"  "[$linux_version]"  "[$date_formatted]"
