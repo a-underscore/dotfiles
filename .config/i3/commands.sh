1ed22ffa2181ea5aa263d1f263881cd577e00de5
Brendan Bortz <brendansbortz@protonmail.com>
2022-10-17 11:23:03 -0400

    better

--- .config/i3/commands.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dmenu_run -i -fn 'pango:DejaVu Sans Mono 12' -sb '#999999' -sf '#ffffff' -nb '#121212' -nf '#ffffff'
--- .config/i3/status.sh
+++ .config/i3/status.sh
@@ -3,6 +3,7 @@
 cpu=$(nproc)
 version=$(uname -r)
 uptime=$(uptime -p)
+battery=$(acpi -b)
 date=$(date +'%-d/%-m/%Y %H:%M:%S')
 
-echo [$cpu] [$version] [$uptime] [$date]
+echo [$cpu] [$version] [$uptime] [$battery] [$date]
